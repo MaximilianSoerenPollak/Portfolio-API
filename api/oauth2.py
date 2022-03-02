70a7fba397d3c5fc5f84fe27447c72b91081c8c6
MaximilianSoerenPollak <pollakmaximilian@gmail.com>
2022-03-03 00:51:31 +0100

    integrated  auth. into create route.

---
+++ api/oauth2.py
@@ -0,0 +1,42 @@
+from jose import JWTError, jwt
+from decouple import config
+from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+SECRET_KEY = config("JWT_SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE = 60
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+        return token_data
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-authenticate": "Bearer"},
+    )
+    return verify_access_token(token, credentials_exception)
--- api/routers/stock.py
+++ api/routers/stock.py
@@ -1,6 +1,6 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 from typing import List
 
@@ -15,7 +15,10 @@ def get_stocks(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.StockResponse)
-def create_stock(stock: schemas.StockCreate, db: Session = Depends(get_db)):
+def create_stock(
+    stock: schemas.StockCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)
+):
+    # !Todo: make a flag somehow in this stock so the stocks are marked as non searchable or "user_added" or something.
     new_stock = models.Stock(**stock.dict())
     db.add(new_stock)
     db.commit()
@@ -32,7 +35,8 @@ def get_stock(id: int, response: Response, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_stock(id: int, db: Session = Depends(get_db)):
+def delete_stock(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+    # !Todo only admin can delete.
     stock = db.query(models.Stock).filter(models.Stock.id == id)
     if stock.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Stock with ID: {id} does not exist")
